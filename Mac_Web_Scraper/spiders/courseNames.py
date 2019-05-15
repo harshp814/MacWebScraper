3c9a77172b087d58154e6bb32de654781eb45e49
Harsh Patel <patelh11@mcmaster.ca>
2019-05-14 22:01:33 -0400

    It can scrape all the names, need to write regex to extract and create objects

--- Mac_Web_Scraper/spiders/courseNames.py
+++ Mac_Web_Scraper/spiders/courseNames.py
@@ -1,6 +1,7 @@
 import scrapy
 from selenium import webdriver
 import json
+import re
 
 class CourseNamesSpider(scrapy.Spider):
     name = "coursenames"
@@ -16,14 +17,19 @@ class CourseNamesSpider(scrapy.Spider):
         # self.driver = webdriver.Chrome()
 
     def parse(self, response):
-        departments = self.readDepartments()
+        # departments = self.readDepartments()      //table[@class='table_default'][2]/tbody/tr/td/a/
 
         # for i in departments:
-        anchorTags = response.xpath("//table[@class='table_default'][2]/tbody/tr/td/a")
+        anchorTags = response.xpath("//table[@class='table_default'][2]/tr[position()>2 and position()<last()]/td[2]/a/text()").extract()
+        courseNames = {}
         for i in range(len(anchorTags)):
-            fullName = anchorTags[i].xpath("text()").extract_first()
-            print (fullName)
+            yield {
+                "fullName" : anchorTags[i],
+                "department" : re.search("^[\w]*", anchorTags[i]),
+                "courseCode" : re.search("\b[A-Z0-9]{4}\b", anchorTags[i])
+            }       
             # url = self.changeInputs(i, departments[i])
+        
 
     def readDepartments(self):
         with open("departments.json", "r") as f:
